935d75067a231b6e9917189310bfdfb06a48c0d7
Najam Ul Saqib <1najamulsaqib@gmail.com>
2021-04-26 22:24:18 +0500

    v1.1.111

---
+++ app/src/main/java/com/example/freshmanfinal/DepartmentHead.java
@@ -0,0 +1,14 @@
+package com.example.freshmanfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepartmentHead extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department_head);
+    }
+}
